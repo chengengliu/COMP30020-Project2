387895ecb8f80a6712a57c8491b1ecf6037b4174
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-10-02 12:56:21 +1000

    Change file, move from starter to project2.pl

---
+++ project2.pl
@@ -0,0 +1,74 @@
+% You can use this code to get started with your fillin puzzle solver.
+% Make sure you replace this comment with your own documentation.
+
+:- ensure_loaded(library(clpfd)).
+:- use_module(file_reader).
+
+main(PuzzleFile, WordlistFile, SolutionFile) :-
+	read_file(PuzzleFile, Puzzle),
+	read_file(WordlistFile, Wordlist),
+	valid_puzzle(Puzzle),
+	solve_puzzle(Puzzle, Wordlist, Solved),
+	print_puzzle(SolutionFile, Solved).
+
+% solve_puzzle(Puzzle0, WordList, Puzzle)
+% should hold when Puzzle is a solved version of Puzzle0, with the
+% empty slots filled in with words from WordList.  Puzzle0 and Puzzle
+% should be lists of lists of characters (single-character atoms), one
+% list per puzzle row.  WordList is also a list of lists of
+% characters, one list per word.
+%
+% This code is obviously wrong: it just gives back the unfilled puzzle
+% as result.  You'll need to replace this with a working
+% implementation.
+
+solve_puzzle(Puzzle, _, Puzzle).
+
+
+
+% TODO: Need to change to tail recursive to improve efficiency. 
+qsort([],[]).
+qsort([H|T], Sorted):-
+    qpartition(H, T, Less, Greater),
+    qsort(Less, SortedLess),
+    qsort(Greater, SortedGreater),
+    append(SortedLess, [H|SortedGreater], Sorted).
+
+
+qpartition(_,[],[],[]).
+qpartition(Pivot, [E|Rest], [E|Less], Greater):-
+    E =< Pivot,
+    qpartition(Pivot, Rest, Less, Greater).
+qpartition(Pivot, [E|Rest], Less, [E|Greater]):-
+    E > Pivot,
+    qpartition(Pivot, Rest, Less, Greater).
+
+
+/**This is the print out part.  
+ * TODO: Maybe later separated into another file.
+ * 
+ * 
+ * */
+print_puzzle(SolutionFile, Puzzle) :-
+	open(SolutionFile, write, Stream),
+	maplist(print_row(Stream), Puzzle),
+	close(Stream).
+
+print_row(Stream, Row) :-
+	maplist(put_puzzle_char(Stream), Row),
+	nl(Stream).
+
+put_puzzle_char(Stream, Char) :-
+	(   var(Char)
+	->  put_char(Stream, '_')
+	;   put_char(Stream, Char)
+	).
+
+valid_puzzle([]).
+valid_puzzle([Row|Rows]) :-
+	maplist(samelength(Row), Rows).
+
+
+samelength([], []).
+samelength([_|L1], [_|L2]) :-
+	same_length(L1, L2).
