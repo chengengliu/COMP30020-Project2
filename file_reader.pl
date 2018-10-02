80c962f8485b81e42c95729520e8a3a93c7043a3
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-10-02 12:52:58 +1000

    Add sorting algorithm

---
+++ file_reader.pl
@@ -0,0 +1,30 @@
+:-  module(file_reader, [read_file/2]).
+
+
+read_file(Filename, Content) :-
+	open(Filename, read, Stream),
+	read_lines(Stream, Content),
+	close(Stream).
+
+read_lines(Stream, Content) :-
+	read_line(Stream, Line, Last),
+	(   Last = true
+	->  (   Line = []
+	    ->  Content = []
+	    ;   Content = [Line]
+	    )
+	;  Content = [Line|Content1],
+	    read_lines(Stream, Content1)
+	).
+
+read_line(Stream, Line, Last) :-
+	get_char(Stream, Char),
+	(   Char = end_of_file
+	->  Line = [],
+	    Last = true
+	; Char = '\n'
+	->  Line = [],
+	    Last = false
+	;   Line = [Char|Line1],
+	    read_line(Stream, Line1, Last)
+	).
--- proj2_starter.pl
+++ proj2_starter.pl
@@ -1,6 +1,10 @@
 % You can use this code to get started with your fillin puzzle solver.
 % Make sure you replace this comment with your own documentation.
 
+:- include('project2.pl').
+:- ensure_loaded(library(clpfd)).
+:- use_module(file_reader).
+
 main(PuzzleFile, WordlistFile, SolutionFile) :-
 	read_file(PuzzleFile, Puzzle),
 	read_file(WordlistFile, Wordlist),
@@ -8,34 +12,44 @@ main(PuzzleFile, WordlistFile, SolutionFile) :-
 	solve_puzzle(Puzzle, Wordlist, Solved),
 	print_puzzle(SolutionFile, Solved).
 
-read_file(Filename, Content) :-
-	open(Filename, read, Stream),
-	read_lines(Stream, Content),
-	close(Stream).
+% solve_puzzle(Puzzle0, WordList, Puzzle)
+% should hold when Puzzle is a solved version of Puzzle0, with the
+% empty slots filled in with words from WordList.  Puzzle0 and Puzzle
+% should be lists of lists of characters (single-character atoms), one
+% list per puzzle row.  WordList is also a list of lists of
+% characters, one list per word.
+%
+% This code is obviously wrong: it just gives back the unfilled puzzle
+% as result.  You'll need to replace this with a working
+% implementation.
 
-read_lines(Stream, Content) :-
-	read_line(Stream, Line, Last),
-	(   Last = true
-	->  (   Line = []
-	    ->  Content = []
-	    ;   Content = [Line]
-	    )
-	;  Content = [Line|Content1],
-	    read_lines(Stream, Content1)
-	).
+solve_puzzle(Puzzle, _, Puzzle).
 
-read_line(Stream, Line, Last) :-
-	get_char(Stream, Char),
-	(   Char = end_of_file
-	->  Line = [],
-	    Last = true
-	; Char = '\n'
-	->  Line = [],
-	    Last = false
-	;   Line = [Char|Line1],
-	    read_line(Stream, Line1, Last)
-	).
 
+
+% TODO: Need to change to tail recursive to improve efficiency. 
+qsort([],[]).
+qsort([H|T], Sorted):-
+    qpartition(H, T, Less, Greater),
+    qsort(Less, SortedLess),
+    qsort(Greater, SortedGreater),
+    append(SortedLess, [H|SortedGreater], Sorted).
+
+
+qpartition(_,[],[],[]).
+qpartition(Pivot, [E|Rest], [E|Less], Greater):-
+    E =< Pivot,
+    qpartition(Pivot, Rest, Less, Greater).
+qpartition(Pivot, [E|Rest], Less, [E|Greater]):-
+    E > Pivot,
+    qpartition(Pivot, Rest, Less, Greater).
+
+
+/**This is the print out part.  
+ * TODO: Maybe later separated into another file.
+ * 
+ * 
+ * */
 print_puzzle(SolutionFile, Puzzle) :-
 	open(SolutionFile, write, Stream),
 	maplist(print_row(Stream), Puzzle),
@@ -59,17 +73,3 @@ valid_puzzle([Row|Rows]) :-
 samelength([], []).
 samelength([_|L1], [_|L2]) :-
 	same_length(L1, L2).
-
-
-% solve_puzzle(Puzzle0, WordList, Puzzle)
-% should hold when Puzzle is a solved version of Puzzle0, with the
-% empty slots filled in with words from WordList.  Puzzle0 and Puzzle
-% should be lists of lists of characters (single-character atoms), one
-% list per puzzle row.  WordList is also a list of lists of
-% characters, one list per word.
-%
-% This code is obviously wrong: it just gives back the unfilled puzzle
-% as result.  You'll need to replace this with a working
-% implementation.
-
-solve_puzzle(Puzzle, _, Puzzle).
